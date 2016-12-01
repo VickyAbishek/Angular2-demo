dbb17936a01462893a0e67a08afcf7acb93fe84f
VickyAbishek <vicky.abishek@outlook.com>
2016-12-01 12:29:03 +0530

    Initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+//import { ProductListComponent } from './product/product-list.component'; --> not necessary
+@Component ({
+    selector: 'pm-app',
+    template: `
+    <h1> {{pageTitle}} <h1>
+     <pm-products>sdaf</pm-products>
+    `
+})
+export class AppComponent {
+    pageTitle: string = "from Component 1 ";
+}
---
+++ app/product/product-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'pm-products',
+    templateUrl:
+    './product/product-list.component.html'
+})
+
+export class ProductListComponent 
+{
+
+}
